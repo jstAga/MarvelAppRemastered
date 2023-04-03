907a36657ec4afb96bcb60a71d73ab7a99724553
Aktan <86038270+jstAga@users.noreply.github.com>
2023-04-03 21:46:39 +0600

    ADD flowFragments

---
+++ app/src/main/java/com/example/marvelappremastered/SignFlowFragment.kt
@@ -0,0 +1,7 @@
+package com.example.marvelappremastered
+
+import com.example.marvelappremastered.ui.core.base.BaseFlowFragment
+
+class SignFlowFragment : BaseFlowFragment(R.layout.fragment_sign_flow, R.id.signFlowFragment) {
+
+}
--- app/src/main/java/com/example/marvelappremastered/ui/fragments/onBoarding/OnBoardingFragment.kt
+++ app/src/main/java/com/example/marvelappremastered/ui/fragments/onBoarding/OnBoardingFragment.kt
@@ -7,7 +7,7 @@ import com.example.marvelappremastered.ui.core.base.BaseFragment
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class OnBoardingFragment :
-    BaseFragment<FragmentOnBoardingBinding, OnBoardingViewModel>(R.layout.fragment_auth) {
+    BaseFragment<FragmentOnBoardingBinding, OnBoardingViewModel>(R.layout.fragment_on_boarding) {
 
     override val binding by viewBinding(FragmentOnBoardingBinding::bind)
     override val viewModel by viewModel<OnBoardingViewModel>()
