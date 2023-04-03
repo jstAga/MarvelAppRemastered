f6af672bb070463524e8b7435b3be214ef185f7c
Aktan <86038270+jstAga@users.noreply.github.com>
2023-04-03 18:08:51 +0600

    ADD home,comics,shop,onBoarding screens

--- app/src/main/java/com/example/marvelappremastered/ui/fragments/splashScreen/SplashScreenFragment.kt
+++ app/src/main/java/com/example/marvelappremastered/ui/fragments/splashScreen/SplashScreenFragment.kt
@@ -1,14 +1,39 @@
 package com.example.marvelappremastered.ui.fragments.splashScreen
 
+import android.annotation.SuppressLint
+import android.os.Handler
+import android.os.Looper
+import androidx.navigation.fragment.findNavController
 import by.kirich1409.viewbindingdelegate.viewBinding
+import coil.load
 import com.example.marvelappremastered.R
 import com.example.marvelappremastered.databinding.FragmentSplashScreenBinding
 import com.example.marvelappremastered.ui.core.base.BaseFragment
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
+@SuppressLint("CustomSplashScreen")
 class SplashScreenFragment :
     BaseFragment<FragmentSplashScreenBinding, SplashScreenViewModel>(R.layout.fragment_auth) {
 
     override val binding by viewBinding(FragmentSplashScreenBinding::bind)
     override val viewModel by viewModel<SplashScreenViewModel>()
+
+    override fun initialize() {
+        super.initialize()
+        binding.ivLogo.load(R.drawable.ic_marvel_logo) {
+            crossfade(750)
+            build()
+        }
+        afterSplash()
+    }
+
+    private fun afterSplash() {
+        Handler(Looper.getMainLooper()).postDelayed({
+            //            if (auth.currentUser?.uid != null) {
+            findNavController().navigate(R.id.homeFragment)
+            //            } else {
+            //                findNavController().navigate(R.id.boardingFragment)
+            //            }
+        }, 3000)
+    }
 }
